da63b85211528aab9f550c764a8c270cbd9b0575
daveproyt-ui <dajacago19882@gmail.com>
2026-06-27 22:54:00 -0500

    Update database.py

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -9,6 +9,7 @@ class Database:
             "DATABASE=proyecto2DB;"
             "Trusted_Connection=yes;"
         )
+        
     async def ejecutar(self, sql, parametros=()):
         await asyncio.sleep(0.1)
         def consulta():
@@ -23,3 +24,17 @@ class Database:
                 # 🚨 EL DETECTIVE: Esto imprimirá letras rojas en la terminal si SQL falla
                 print(f"❌ Error SQL silencioso detectado: {e}")
         await asyncio.to_thread(consulta)
+        
+    async def consultar(self, sql, parametros=()):
+        await asyncio.sleep(1)
+        def consulta():
+            conexion = pyodbc.connect(self.cadena_conexion)
+            cursor = conexion.cursor()
+            cursor.execute(sql, parametros)
+            datos = cursor.fetchall()
+            conexion.commit() # 🚨 El commit que salva los datos
+            cursor.close()
+            conexion.close()
+            return datos
+        return await asyncio.to_thread(consulta)
+        
